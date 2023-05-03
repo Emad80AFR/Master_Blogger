f2f3e36280e0be56f6efc44e4d8332093664da3c
emad80AFR <emad.80.afrasiyabi@gmail.com>
2023-05-03 06:08:51 +0330

    FrameWork and DomainBase Class added to project

--- Master_Blogger/MB.Domain/ArticleCategoryAgg/ArticleCategory.cs
+++ Master_Blogger/MB.Domain/ArticleCategoryAgg/ArticleCategory.cs
@@ -1,14 +1,13 @@
-﻿using MB.Domain.ArticleAgg;
+﻿using FrameWork.Domain;
+using MB.Domain.ArticleAgg;
 using MB.Domain.ArticleCategoryAgg.Services;
 
 namespace MB.Domain.ArticleCategoryAgg
 {
-    public class ArticleCategory
+    public class ArticleCategory:DomainBase<long>
     {
-        public long Id { get; private set; }
         public string Title { get; private set; }
         public bool IsDeleted { get; private set; }
-        public DateTime CreationDate { get; private set; }
         public ICollection<Article> Articles { get; private set; }
 
         protected ArticleCategory() { }
@@ -18,7 +17,6 @@ namespace MB.Domain.ArticleCategoryAgg
             validatorService.CheckThatThisRecordIsAlreadyExist(Title!);
 
             Title = title;
-            CreationDate=DateTime.Now;
             IsDeleted = default;
             Articles = new List<Article>();
         }
